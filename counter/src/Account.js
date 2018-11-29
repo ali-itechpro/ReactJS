7ea6f24c0da5f8c150ee7ff91c0dd2f63c9b60de
ali.itechpro <ali.itechpro@gmailcom>
2018-11-29 12:04:35 +0530

    counter and account example folder

---
+++ counter/src/Account.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+
+class Account extends Component {
+    constructor(){
+      super();
+      
+      this.state={
+          myBalance:0,
+          alert:''
+      }
+    }
+
+    Deposit = () => {
+        if (this.state.myBalance < 100) {
+            this.setState({
+                myBalance: this.state.myBalance + 10
+               
+            })
+        }
+        else if(this.state.myBalance ==100){
+            this.setState({
+                alert: "Limited Finished"
+            })
+        }
+
+    }
+    Withdraw=()=>{
+
+        if(this.state.myBalance>=0){
+            this.setState({
+                myBalance:this.state.myBalance-10,
+                alert:''
+                
+            })
+
+            if(this.state.myBalance==0){
+                this.setState({
+                    alert:'Balance Finished'
+                })
+            }
+
+    
+    }
+}
+    
+ styles={
+     width:'300px', 
+     height:300,
+     
+
+ }
+
+    render(){
+        return(
+            <div className="text-center">
+            <br/>
+                <h1>Welcome to My Account</h1>
+
+                <div className="bg-default text-center border" style={this.styles}>
+                <h2>Your Balance</h2>
+                <h3 className="text-danger">{this.state.alert}</h3>
+                <br/>
+                <h1 className="text-warning">$ {this.state.myBalance}</h1>
+                <button className="btn btn-primary m-2" onClick={this.Deposit}>Deposit 10$</button>
+                <button className="btn btn-danger" onClick={this.Withdraw}>Withdraw -10$</button>
+                </div>
+            
+            
+            
+            </div>
+        )
+    }
+
+}
+
+export default Account;
---
+++ counter/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Account from './Account'
+
+class App extends Component {
+ constructor(){
+   super();
+   this.state={
+     count:0
+   }
+
+
+ }
+
+Increament=()=>{
+  this.setState({
+    count:this.state.count+1
+  })
+
+}
+
+Decreament=()=>{
+  this.setState({
+    count:this.state.count-1
+  })
+
+}
+
+
+
+  render() {
+    return (
+      <div className="App">
+       
+        <h1>COUNTER</h1>
+        <h2 className="text-warning">{this.state.count}</h2>
+        <button onClick={this.Increament} className="btn btn-primary m-2">Increasement</button>
+        <button onClick={this.Decreament} className="btn btn-primary">Decreament</button>
+
+      <hr/>
+      
+      <Account/>
+      
+
+      </div>
+    );
+  }
+}
+
+export default App;
